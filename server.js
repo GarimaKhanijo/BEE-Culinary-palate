0cc4a0c11da811743b4afe8fa0f66983a47996d7
Garima <garima313.be22@chitkara.edu.in>
2024-12-09 10:29:13 +0530

    Mongodb compass->atlas

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+// const cors = require('cors');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-
+// app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -18,18 +19,32 @@ app.use('/api/recipes', recipeRoutes);
 app.use('/api/users', userRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
+// connectDB().then(() => {
+//     console.log("connected to db");
+//     app.listen(PORT, () => {
+//         console.log(`Server is running on http://localhost:${PORT}`);
+//     });
+// }).catch(err => console.log(err));
+
+// async function connectDB() {
+//     await mongoose.connect('mongodb://127.0.0.1:27017/Culinary');
+//     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/Culinary');` if your database has auth enabled
+// }
+
+async function connectDB() {
+    try {
+        await mongoose.connect('mongodb+srv://garima290105:garima2905@cluster0.ota8d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        console.log("Connected to MongoDB Atlas");
+    } catch (err) {
+        console.error("Failed to connect to MongoDB Atlas", err);
+    }
+}
+
 connectDB().then(() => {
     console.log("connected to db");
     app.listen(PORT, () => {
         console.log(`Server is running on http://localhost:${PORT}`);
     });
 }).catch(err => console.log(err));
-
-async function connectDB() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/Culinary');
-    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/Culinary');` if your database has auth enabled
-}
-
-
 
 
